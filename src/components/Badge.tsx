93a6d11bdf44cb83fb69d7ff25975c37f5f01a6e
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2024-03-21 16:28:58 +0200

    feat: add Badge component

---
+++ src/components/Badge.tsx
@@ -0,0 +1,17 @@
+/* eslint-disable */
+
+import React from 'react';
+
+interface BadgeProps extends React.AnchorHTMLAttributes<HTMLAnchorElement> {}
+
+const Badge = (props: BadgeProps) => {
+  return (
+    <a
+      {...props}
+      target="_blank"
+      className="inline-flex items-center rounded border border-neutral-200 bg-neutral-50 p-1 text-sm leading-4 text-neutral-900 no-underline dark:border-neutral-700 dark:bg-neutral-800 dark:text-neutral-100"
+    />
+  );
+};
+
+export default Badge;
