cc81d050e5f53871b216d8c51ae1e9a7cd50b55a
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2024-03-21 16:55:38 +0200

    fix: only display All Projects for pathname projects

--- src/components/PageSection.tsx
+++ src/components/PageSection.tsx
@@ -21,9 +21,10 @@ export const PageSection = ({
   const pathname = usePathname();
   return (
     <>
-      {pathname === '/' ? (
+      {pathname === '/' && (
         <span className="font-semibold md:px-6">{title}</span>
-      ) : (
+      )}
+      {pathname === '/projects' && (
         <span className="text-4xl font-bold md:px-6 mb-6 md:mb-4">
           All Projects
         </span>
