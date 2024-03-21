93f2e2fc4b0f1161ef8641fc2d03255bf9fc3811
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2024-03-21 17:45:20 +0200

    fix: update open graph bg image url

--- src/app/og/route.tsx
+++ src/app/og/route.tsx
@@ -21,7 +21,7 @@ export async function GET(req: NextRequest) {
           flexDirection: 'column',
           alignItems: 'flex-start',
           justifyContent: 'center',
-          backgroundImage: 'url(https://hosnaqasmei.com/og-bg.png)',
+          backgroundImage: 'url(/og-bg.png)',
         }}
       >
         <div
