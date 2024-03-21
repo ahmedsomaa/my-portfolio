9121db6a5a7ae78b980585be21e34bc2ba239a93
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2024-03-21 20:49:56 +0200

    docs: add ecard app & store api projects

--- src/config/projects.tsx
+++ src/config/projects.tsx
@@ -15,6 +15,23 @@ export const PROJECTS = [
     image: '/projects/editor-setup.svg',
     link: 'https://editorsetup.vercel.app',
   },
+  {
+    featured: true,
+    name: 'StoreAPI',
+    slug: 'store-api',
+    description: 'A node API for a store built with Typescript & PostgreSQL.',
+    image: '/projects/store-api.svg',
+    link: 'https://github.com/ahmedsomaa/storefront-api',
+  },
+  {
+    featured: false,
+    name: 'ECARD Mobile App',
+    slug: 'ecard-app',
+    description:
+      "An app to streamline the process of fertilizers' distribution.",
+    image: '/projects/ecard-app.svg',
+    link: 'https://play.google.com/store/apps/details?id=com.newsmartegypt.ecard&pli=1',
+  },
 ];
 
 export interface IProject {
