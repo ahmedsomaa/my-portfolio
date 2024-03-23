e6b09e66a964f86b180f1ba2815e3cff36d4b19a
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2024-03-23 18:55:21 +0200

    style: add borders to user avatar

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -10,13 +10,15 @@ export default function Header() {
   return (
     <header className="flex flex-col md:flex-row md:justify-between md:items-center space-y-4 md:space-y-0 w-full md:px-6 max-w-3xl">
       <div className="flex flex-row items-center space-x-3">
-        <Image
-          width={100}
-          height={100}
-          src={CONFIG.profile.headshot}
-          alt="profile headshot for Ahmed Abu Qahf"
-          className="rounded-full h-[48px] w-[48px]"
-        />
+        <div className="rounded-full bg-gradient-to-br from-zinc-700/60 to-zinc-500/60 shadow-lg p-[3px] group transform transition ease-out hover:scale-105 hover:from-zinc-700 hover:to-zinc-400 hover:zinc-rose-500/25 active:translate-y-px ring-[5px] ring-zinc-500/10">
+          <Image
+            width={100}
+            height={100}
+            src={CONFIG.profile.headshot}
+            alt="profile headshot for Ahmed Abu Qahf"
+            className="rounded-full h-[48px] w-[48px]"
+          />
+        </div>
         <div className="flex flex-col">
           <span className="font-semibold">{CONFIG.profile.name}</span>
           <span className="text-secondaryDarker">{CONFIG.profile.title}</span>
