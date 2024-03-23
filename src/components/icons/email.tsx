a471bac088caefd06f00ad98740d50766db4b2e6
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2024-03-23 15:22:34 +0200

    feat: add email icon

---
+++ src/components/icons/email.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+export const EmailIcon = () => {
+  return (
+    <svg
+      fill="none"
+      width="24px"
+      height="24px"
+      strokeWidth="1.5"
+      viewBox="0 0 24 24"
+      color="currentColor"
+      xmlns="http://www.w3.org/2000/svg"
+      className="text-secondaryDarker group-hover:text-secondaryDark duration-200"
+    >
+      <path
+        d="M9 9L13.5 12L18 9"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></path>
+      <path
+        d="M3 13.5H5"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></path>
+      <path
+        d="M1 10.5H5"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></path>
+      <path
+        d="M5 7.5V7C5 5.89543 5.89543 5 7 5H20C21.1046 5 22 5.89543 22 7V17C22 18.1046 21.1046 19 20 19H7C5.89543 19 5 18.1046 5 17V16.5"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+      ></path>
+    </svg>
+  );
+};
