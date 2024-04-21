09502b60d5ee0536b04b81b0aaa56b7c7e2249e3
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2024-04-21 21:06:45 +0200

    feat: add posts filter data

---
+++ src/config/posts.tsx
@@ -0,0 +1,23 @@
+export const POSTS_FILTER = [
+  { tag: null, name: 'all', href: '/posts' },
+  {
+    tag: 'technical',
+    name: 'technical',
+    href: '/posts?tag=technical',
+  },
+  {
+    tag: 'personal',
+    name: 'personal',
+    href: '/posts?tag=personal',
+  },
+  {
+    tag: 'tips',
+    name: 'tips and tricks',
+    href: '/posts?tag=tips',
+  },
+  {
+    tag: 'random',
+    name: 'random',
+    href: '/posts?tag=random',
+  },
+];
