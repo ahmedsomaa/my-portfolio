3d4a3200aee09083054f58a3306b611fe76a0d0d
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2024-04-21 21:16:51 +0200

    refactor: Use post filters from CONFIG

--- src/app/posts/posts.tsx
+++ src/app/posts/posts.tsx
@@ -5,8 +5,8 @@ import React from 'react';
 import Link from 'next/link';
 import { useSearchParams } from 'next/navigation';
 
-import { ArrowLeftIcon } from '@/components/icons/arrow-left';
 import { PostCard } from '@/components/PostCard';
+import CONFIG from '@/config';
 import { IPost } from '@/lib/posts';
 
 export default function Posts({
@@ -17,56 +17,28 @@ export default function Posts({
   views?: any;
 }) {
   const searchParams = useSearchParams();
-  const tag = searchParams.get('tag');
+  const _tag = searchParams.get('tag');
 
-  const filteredPosts = tag
-    ? allPosts.filter((post: any) => post.metadata.tag.includes(tag))
+  const filteredPosts = _tag
+    ? allPosts.filter((post: any) => post.metadata.tag.includes(_tag))
     : allPosts;
 
   return (
-    <div className="grid grid-cols-1 gap-10 pb-10 w-full">
+    <div className="grid grid-cols-1 gap-10 w-full">
       <div className="flex flex-col">
-        <Link
-          href="/"
-          className="flex flex-row space-x-2 items-center md:px-6 group cursor-pointer mb-4"
-        >
-          <ArrowLeftIcon />
-          <span className="text-secondaryDarker">Back</span>
-        </Link>
-        <span className="text-4xl font-bold md:px-6 mb-6 md:mb-4">
+        <span className="text-2xl font-bold md:px-6 mb-6 md:mb-4 lowercase">
           All Posts
         </span>
         <div className="flex flex-row space-x-4 mb-6 md:mb-4 text-sm  md:px-6 overflow-x-auto line-clamp-1 text-nowrap">
-          <Link
-            href="/posts"
-            className={`${tag === null ? 'text-secondaryDark underline' : 'text-secondaryDarker'} hover:text-secondaryDark duration-200 hover:underline`}
-          >
-            All
-          </Link>
-          <Link
-            href="/posts?tag=technical"
-            className={`${tag === 'technical' ? 'text-secondaryDark underline' : 'text-secondaryDarker'} hover:text-secondaryDark duration-200 hover:underline`}
-          >
-            Technical
-          </Link>
-          <Link
-            href="/posts?tag=personal"
-            className={`${tag === 'personal' ? 'text-secondaryDark underline' : 'text-secondaryDarker'} hover:text-secondaryDark duration-200 hover:underline`}
-          >
-            Personal
-          </Link>
-          <Link
-            href="/posts?tag=tips"
-            className={`${tag === 'book-notes' ? 'text-secondaryDark underline' : 'text-secondaryDarker'} hover:text-secondaryDark duration-200 hover:underline `}
-          >
-            Hacks and Tips
-          </Link>
-          <Link
-            href="/posts?tag=random"
-            className={`${tag === 'random' ? 'text-secondaryDark underline' : 'text-secondaryDarker'} hover:text-secondaryDark duration-200 hover:underline`}
-          >
-            Random
-          </Link>
+          {CONFIG.postsFilters.map(({ tag, href, name }) => (
+            <Link
+              key={name}
+              href={href}
+              className={`${_tag === tag ? 'text-secondaryDark underline' : 'text-secondaryDarker'} hover:text-secondaryDark duration-200 hover:underline`}
+            >
+              {name}
+            </Link>
+          ))}
         </div>
 
         <div>
