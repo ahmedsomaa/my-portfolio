9905f313811e4fbaa191b5c52b01f5c986ed6a72
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2024-04-21 21:06:29 +0200

    refactor: Add logoText property

--- src/config/profile.tsx
+++ src/config/profile.tsx
@@ -1,44 +1,56 @@
-import { EmailIcon } from '@/components/icons/email';
-import { GithubIcon } from '@/components/icons/github';
-import { HashnodeIcon } from '@/components/icons/hashnode';
-import { LinkedInIcon } from '@/components/icons/linkedin';
-import { ResumeIcon } from '@/components/icons/resume';
-import { XIcon } from '@/components/icons/x';
-import { YouTubeIcon } from '@/components/icons/youtube';
+import { EmailIcon } from '@/components/icons/socials/email';
+import { GithubIcon } from '@/components/icons/socials/github';
+import { HashnodeIcon } from '@/components/icons/socials/hashnode';
+import { LinkedInIcon } from '@/components/icons/socials/linkedin';
+import { ResumeIcon } from '@/components/icons/socials/resume';
+import { XIcon } from '@/components/icons/socials/x';
+import { YouTubeIcon } from '@/components/icons/socials/youtube';
 
 export const PROFILE = {
+  logoText: 'Som3aware',
   name: 'Ahmed Abu Qahf',
   headshot: '/avatar.webp',
   title: 'Software Engineer',
   description: `Hi, I'm Ahmed Abu Qahf. I'm based in Tanta, Egypt.`,
   socials: [
     {
+      id: 'gmail',
       platform: 'Mail',
       link: 'mailto:abokahfa@gmail.com',
       icon: <EmailIcon />,
     },
-    { platform: 'X', link: 'https://twitter.com/som3aware', icon: <XIcon /> },
     {
+      id: 'twitter',
+      platform: 'X',
+      link: 'https://twitter.com/som3aware',
+      icon: <XIcon />,
+    },
+    {
+      id: 'github',
       platform: 'GitHub',
       link: 'https://github.com/ahmedsomaa',
       icon: <GithubIcon />,
     },
     {
+      id: 'youtube',
       platform: 'YouTube',
       link: 'https://www.youtube.com/@the-final-lineup',
       icon: <YouTubeIcon />,
     },
     {
+      id: 'linkedin',
       platform: 'LinkedIn',
       link: 'https://www.linkedin.com/in/som3aware/',
       icon: <LinkedInIcon />,
     },
     {
+      id: 'hashnode',
       platform: 'Hashnode',
       link: 'https://som3aware.hashnode.dev/',
       icon: <HashnodeIcon />,
     },
     {
+      id: 'resume',
       platform: 'My Resume',
       link: '/docs/my-resume.pdf',
       icon: <ResumeIcon />,
