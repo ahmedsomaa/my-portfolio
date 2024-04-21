35672f6ddf53c3f037795abc8b9fdd120f3f5014
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2024-04-21 21:02:45 +0200

    refactor: Remove back button & decrease headings size to 2xl and paragraphs to sm

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,41 +1,33 @@
-import Link from 'next/link';
+import type { Metadata } from 'next';
 
 import { ExperienceCard } from '@/components/ExperienceCard';
 import AboutMe from '@/components/home/AboutMe';
-import { ArrowLeftIcon } from '@/components/icons/arrow-left';
 import CONFIG from '@/config';
 
-export const metadata = {
+export const metadata: Metadata = {
   title: 'About',
-  description: 'Here are more details about me.',
+  description: 'Find more about me',
 };
 
 export default function AboutPage() {
   return (
     <div className="grid grid-cols-1 gap-10 pb-10 w-full">
       <div className="flex flex-col">
-        <Link
-          href="/"
-          className="flex flex-row space-x-2 items-center md:px-6 group cursor-pointer mb-4"
-        >
-          <ArrowLeftIcon />
-          <span className="text-secondaryDarker">Back</span>
-        </Link>
-        <div className="space-y-4">
-          <div className="space-y-2">
-            <span className="text-4xl font-bold md:px-6 mb-6 md:mb-4">
+        <div className="space-y-6">
+          <div className="space-y-4">
+            <h2 className="text-2xl font-bold md:px-6 mb-6 md:mb-4 lowercase">
               About me
-            </span>
+            </h2>
             <div className="md:px-6">
               <AboutMe />
             </div>
           </div>
-          <div className="space-y-2">
-            <span className="text-4xl font-bold md:px-6 mb-6 md:mb-4">
+          <div className="space-y-4">
+            <h2 className="text-2xl font-bold md:px-6 mb-6 md:mb-4 lowercase">
               Experience
-            </span>
+            </h2>
             <div className="md:px-6">
-              <div className="flex flex-col space-y-8 md:space-y-1">
+              <div className="flex flex-col space-y-8 md:space-y-2">
                 {CONFIG.experience.map((experience) => {
                   return (
                     <ExperienceCard
