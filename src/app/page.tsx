d74ba4eca2d38caddb66d557e0d93af6f02548ed
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2024-04-21 21:14:55 +0200

    refactor: Decrease display font sizes to 4xl & 2xl and change text to lowercase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,22 @@
+import { Footer } from '@/components/footer';
 import { BentoGridSection } from '@/components/home/BentoGridSection';
-
-import { Footer } from './footer';
-import Header from './header';
 
 export const revalidate = 0;
 
 export default async function Home() {
   return (
     <>
-      <Header />
       <div className="flex flex-col space-y-6 md:space-y-10 pb-10">
         <div className="flex flex-col md:px-6">
           <div className="flex flex-col space-y-2">
-            {/* <AboutMe /> */}
-            <p className="text-4xl text-center">Hey there, I&apos;am Ahmed!</p>
-            <p className="text-4xl text-center">
-              Welcome to my space on the web!
-            </p>
+            <h1 className="text-4xl text-center">
+              hey there, I&apos;m ahmed ismail!
+            </h1>
+            <h2 className="text-2xl text-center lowercase">
+              Welcome to my personal space on the internet!
+            </h2>
           </div>
         </div>
-
         <div className="flex flex-col md:px-6">
           <div className="flex flex-col space-y-2">
             <BentoGridSection />
