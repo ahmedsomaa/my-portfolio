d265170a3f00259115da9ab1d6446092a7127d08
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2024-04-21 21:05:22 +0200

    refactor: Add reading section to items

--- src/components/home/BentoGridSection.tsx
+++ src/components/home/BentoGridSection.tsx
@@ -1,28 +1,19 @@
 import React from 'react';
 
-import Image from 'next/image';
-
-import {
-  InfoIcon,
-  PencilRulerIcon,
-  PocketKnifeIcon,
-  RssIcon,
-} from 'lucide-react';
-
 import { BentoGrid, BentoGridItem } from '../ui/bento-grid';
 
 export const BentoGridSection = () => {
   return (
-    <BentoGrid className="md:auto-rows-[18rem]">
+    <BentoGrid className="md:grid-rows-9 md:grid-cols-9 md:min-h-[30rem]">
       {items.map((item, i) => (
         <BentoGridItem
           key={i}
           href={item.href}
-          icon={item.icon}
           title={item.title}
-          header={item.header}
           className={item.className}
+          titleStyle={item.titleStyle}
           description={item.description}
+          descriptionStyle={item.descriptionStyle}
         />
       ))}
     </BentoGrid>
@@ -31,79 +22,48 @@ export const BentoGridSection = () => {
 
 const items = [
   {
-    href: '/about',
-    title: 'About me',
-    description: 'Find more about who I am',
-    header: (
-      <Image
-        width={0}
-        height={0}
-        sizes="50vw"
-        alt="an image of man wearing a red hoodie with a dark blue jacket smiling"
-        src="/about.webp"
-        className="flex flex-1 w-full h-full min-h-[6rem] rounded"
-      />
-    ),
+    href: '/posts',
+    title: 'Blogs',
+    titleStyle: 'text-lime-300/95',
+    descriptionStyle: 'text-lime-200/95',
+    description: 'Insightful and helpful content curated for you',
     className:
-      'md:col-span-1 dark:bg-gradient-to-b from-stone-500 to-zinc-900 dark:border-stone-500 dark:hover:border-zinc-900',
-    icon: <InfoIcon className="h-4 w-4 text-neutral-50" />,
+      'md:row-start-1 md:row-span-6 md:col-start-1 md:col-span-3 dark:bg-gradient-to-tl dark:from-gray-50/5 dark:to-lime-300/5 hover:border-lime-300/95',
   },
   {
-    title: 'Blog',
-    href: '/posts',
-    description: 'Insightful and helpful content curated for you',
-    header: (
-      <Image
-        width={0}
-        height={0}
-        sizes="50vw"
-        alt="an image of a typewriter with the words to blog or not to blog printed on the paper"
-        src="/blog.webp"
-        className="flex flex-1 w-full h-full min-h-[6rem] rounded"
-      />
-    ),
+    href: '/reads',
+    title: 'Reads',
+    titleStyle: 'text-sky-300/95',
+    descriptionStyle: 'text-sky-200/95',
+    description: "Books I've recently read",
     className:
-      'md:col-span-2 dark:bg-gradient-to-b from-lime-200 to-zinc-900 dark:border-lime-200 dark:hover:border-zinc-900',
-    icon: <RssIcon className="h-4 w-4 text-neutral-200" />,
+      'md:row-start-7 md:row-span-3 md:col-start-1 md:col-span-3 dark:bg-gradient-to-bl dark:from-gray-50/5 dark:to-sky-300/5 hover:border-sky-300/95',
+  },
+  {
+    href: '/about',
+    title: 'About',
+    titleStyle: 'text-red-300/95',
+    descriptionStyle: 'text-red-200/95',
+    description: 'Find more about me',
+    className:
+      'md:row-start-1 md:row-span-9 md:col-start-4 md:col-span-3 dark:bg-gradient-to-b dark:from-gray-50/5 dark:to-red-300/5 hover:border-red-300/95',
+  },
+  {
+    href: '/tools',
+    title: 'Tools',
+    titleStyle: 'text-purple-300/95',
+    descriptionStyle: 'text-purple-200/95',
+    description: 'Checkout my favorite tools',
+    className:
+      'md:row-start-1 md:row-span-3 md:col-start-7 md:col-span-3 dark:bg-gradient-to-tr dark:from-gray-50/5 dark:to-purple-300/5 hover:border-purple-300/95',
   },
   {
     href: '/projects',
     title: 'Projects',
+    titleStyle: 'text-amber-300/95',
+    descriptionStyle: 'text-amber-200/95',
     description: 'A collection of my favorite works',
-    header: (
-      <Image
-        width={0}
-        height={0}
-        sizes="50vw"
-        alt="an image of a reactjs project code"
-        src="/projects.webp"
-        className="flex flex-1 w-full h-full min-h-[6rem] rounded"
-      />
-    ),
     className:
-      'md:col-span-2 dark:bg-gradient-to-b from-cyan-200 to-zinc-900 dark:border-cyan-200 dark:hover:border-zinc-900',
-    icon: <PencilRulerIcon className="h-4 w-4 text-neutral-200" />,
-  },
-  {
-    href: '/toolkit',
-    title: 'Toolkit',
-    description: 'Checkout my favorite tools',
-    header: (
-      <Image
-        width={0}
-        height={0}
-        sizes="50vw"
-        alt="an image displaying the logos of firefox, vscode, starship, bitwarden & httpie"
-        src="/tools.webp"
-        className="flex flex-1 w-full h-full min-h-[6rem] rounded"
-      />
-    ),
-    className:
-      'md:col-span-1 dark:bg-gradient-to-b from-violet-200 to-zinc-900 dark:border-violet-200 dark:hover:border-zinc-900',
-    icon: <PocketKnifeIcon className="h-4 w-4 text-neutral-200" />,
+      'md:row-start-4 md:row-span-6 md:col-start-7 md:col-span-3 dark:bg-gradient-to-br dark:from-gray-50/5 dark:to-amber-300/5 hover:border-amber-300/95',
   },
 ];
-
-// Images copyrights
-// blog => https://www.pexels.com/photo/a-vintage-typewriter-3601081/
-// projects => https://unsplash.com/photos/black-flat-screen-computer-monitor-mZnx9429i94
