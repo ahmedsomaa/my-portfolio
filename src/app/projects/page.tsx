e48211632457b2299ed4e26e2b7e546ce1b365c7
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2024-04-21 21:17:39 +0200

    refactor: Remove back button

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 
-import Link from 'next/link';
-
 import { ProjectSection } from '@/components/home/ProjectSection';
-import { ArrowLeftIcon } from '@/components/icons/arrow-left';
 import CONFIG from '@/config';
 
 export const metadata = {
@@ -13,17 +10,12 @@ export const metadata = {
 
 export default function ProjectsPage() {
   return (
-    <div className="grid grid-cols-1 gap-10 pb-10 w-full">
-      <div className="flex flex-col">
-        <Link
-          href="/"
-          className="flex flex-row space-x-2 items-center md:px-6 group cursor-pointer mb-4"
-        >
-          <ArrowLeftIcon />
-          <span className="text-secondaryDarker">Back</span>
-        </Link>
-        <ProjectSection projects={CONFIG.projects} />
+    <>
+      <div className="grid grid-cols-1 gap-10 pb-10 w-full">
+        <div className="flex flex-col">
+          <ProjectSection projects={CONFIG.projects} />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
