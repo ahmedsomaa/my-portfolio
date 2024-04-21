844c9266e770fda720d4eb0929194da2be4ecbbe
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2024-04-21 21:23:18 +0200

    refactor: Move project section under projects directory

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-import { ProjectSection } from '@/components/home/ProjectSection';
+import { ProjectCard } from '@/components/ProjectCard';
 import CONFIG from '@/config';
 
 export const metadata = {
   title: 'Projects',
-  description: 'Here are all the projects I have built.',
+  description: 'A collection of my favorite works',
 };
 
 export default function ProjectsPage() {
@@ -13,7 +13,14 @@ export default function ProjectsPage() {
     <>
       <div className="grid grid-cols-1 gap-10 pb-10 w-full">
         <div className="flex flex-col">
-          <ProjectSection projects={CONFIG.projects} />
+          <h2 className="text-2xl font-semibold md:px-6 md:mb-4 lowercase">
+            all posts
+          </h2>
+          <div className="flex flex-col space-y-8 md:space-y-1 mdx:px-2">
+            {CONFIG.projects.map((project) => (
+              <ProjectCard key={project.slug} project={project} />
+            ))}
+          </div>
         </div>
       </div>
     </>
