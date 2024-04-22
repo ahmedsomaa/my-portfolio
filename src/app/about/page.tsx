c5397544df0659de7d15f98b7c40f93e8438d03c
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2024-04-22 18:10:22 +0200

    refactor: Add my avatar

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import Image from 'next/image';
 
 import { ExperienceCard } from '@/components/ExperienceCard';
 import AboutMe from '@/components/home/AboutMe';
@@ -11,14 +12,20 @@ export const metadata: Metadata = {
 
 export default function AboutPage() {
   return (
-    <div className="grid grid-cols-1 gap-10 pb-10 w-full md:min-h-[40rem] md:max-h-[40rem] min-h-[48rem] max-h-[48rem]">
+    <div className="grid grid-cols-1 gap-10 pb-10 w-full">
       <div className="flex flex-col">
         <div className="space-y-6">
           <div className="space-y-2">
             <h2 className="text-2xl font-bold text-neutral-50/95 md:px-6 lowercase">
               about me
             </h2>
-            <div className="md:px-6">
+            <div className="md:px-6 flex flex-col items-center gap-2">
+              <Image
+                width={100}
+                height={100}
+                src="/avatar.webp"
+                alt="an avatar image of Ahmed Ismail"
+              />
               <AboutMe />
             </div>
           </div>
