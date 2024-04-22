514e07df5ea334b20f280cc9eaa6ff8c78388bb8
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2024-04-22 03:54:04 +0200

    refactor: Update job titles  & descriptions

--- src/config/experience.tsx
+++ src/config/experience.tsx
@@ -6,42 +6,35 @@ export const EXPERIENCE = [
     companyLogo: '/exp/herwin.webp',
     duration: 'Jan 2024 - Present',
     description:
-      'Working on building the interface for an invoice reconciliation app with Nextjs.',
+      'Working on building the graphical interface for an invoice reconciliation app with Next.js.',
   },
   {
     id: 3,
-    role: 'Software Engineer 2',
+    role: 'Software Engineer II',
     company: 'Nodogoro',
     companyLogo: '/exp/nodogoro.webp',
     duration: 'May 2023 - Dec 2023',
     description:
-      'Worked on building multiple AI apps powered by OpenAI and Next.js.',
+      'Worked on building multiple AI-powered apps using OpenAI, LangChain and Next.js.',
   },
   {
     id: 2,
-    role: 'Software Engineer 1',
+    role: 'Software Engineer I',
     company: 'New Smart Egypt',
     companyLogo: '/exp/nse.webp',
     duration: 'Nov 2021 - May 2023',
     description:
-      'Worked on integrating between vendor softwares (Odoo, Infor, ZKTeco...etc.) using APIs.',
+      'Worked on building web/mobile apps & developing APIs to integrate between multiple systems including Odoo, ZKTeco..etc.',
   },
   {
     id: 1,
     role: 'Software Engineer',
-    company: 'VOIS',
+    company: '_VOIS',
     companyLogo: '/exp/vois.webp',
     duration: 'Jul 2020 - Oct 2021',
     description:
-      'Worked on multiple web apps including HR Management System, Dashboards, Maps...etc.',
+      'Worked on building and providing technical support for multiple web apps including HR Management System, Covid-19 tracker, CRM...etc.',
   },
 ];
 
-export interface IExperience {
-  id: number;
-  role: string;
-  company: string;
-  companyLogo: string;
-  duration: string;
-  description: string;
-}
+export type IExperience = (typeof EXPERIENCE)[0];
