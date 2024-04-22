671914e081fa8c8a6f146646b40b1f268dbf0a51
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2024-04-22 03:49:55 +0200

    ref: Add footer to all pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 
 import '../styles/globals.css';
 
+import { Footer } from '@/components/footer';
 import MaxWidthWrapper from '@/components/max-width-wrapper';
 
 import { Header } from '../components/header';
@@ -66,6 +67,7 @@ export default function RootLayout({
             <div className="flex-1 w-full flex flex-col gap-6 items-center">
               <Header />
               {children}
+              <Footer />
             </div>
           </MaxWidthWrapper>
         </main>
