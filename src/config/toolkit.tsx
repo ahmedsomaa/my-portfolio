b388b3a95f4ab19803cbf6f23b09f9d57a963703
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2024-04-22 03:50:56 +0200

    refactor: Handle hover state

--- src/config/toolkit.tsx
+++ src/config/toolkit.tsx
@@ -16,86 +16,78 @@ export const TOOLKIT = {
       title: 'Firefox',
       icon: <FirefoxIcon />,
       href: 'https://www.mozilla.org/en-US/firefox/',
-      className: 'text-[#FF7139] hover:dark:border-[#FF7139]',
+      className:
+        'text-[#FF7139] hover:dark:border-[#FF7139] hover:bg-gradient-to-br from-gray-50/5 to-[#FF7139]/5',
     },
     {
       title: 'VS Code',
       icon: <VSCodeIcon />,
       href: 'https://code.visualstudio.com/',
-      className: 'text-[#007ACC] hover:dark:border-[#007ACC]',
+      className:
+        'text-[#007ACC] hover:dark:border-[#007ACC] hover:bg-gradient-to-br from-gray-50/5 to-[#007ACC]/5',
     },
     {
       title: 'HTTPie',
       icon: <HttpieIcon />,
       href: 'https://httpie.io/',
-      className: 'text-[#73DC8C] hover:dark:border-[#73DC8C]',
+      className:
+        'text-[#73DC8C] hover:dark:border-[#73DC8C] hover:bg-gradient-to-br from-gray-50/5 to-[#73DC8C]/5',
     },
     {
       title: 'Bitwarden',
       icon: <BitwardenIcon />,
       href: 'https://bitwarden.com/',
-      className: 'text-[#175DDC] hover:dark:border-[#175DDC]',
+      className:
+        'text-[#175DDC] hover:dark:border-[#175DDC] hover:bg-gradient-to-br from-gray-50/5 to-[#175DDC]/5',
     },
     {
       title: 'Starship',
       icon: <StarshipIcon />,
       href: 'https://starship.rs/',
-      className: 'text-[#DD0B78] hover:dark:border-[#DD0B78]',
+      className:
+        'text-[#DD0B78] hover:dark:border-[#DD0B78] hover:bg-gradient-to-br from-gray-50/5 to-[#DD0B78]/5',
     },
     {
       title: 'GitHub',
       icon: <GithubSWIcon />,
       href: 'https://github.com/',
-      className: 'text-[#181717] hover:dark:border-[#181717]',
+      className:
+        'text-neutral-50 hover:dark:border-neutral-50 hover:bg-gradient-to-br from-gray-50/5 to-neutral-50/5',
     },
     {
       title: 'Figma',
       icon: <FigmaIcon />,
       href: 'https://www.figma.com/',
-      className: 'text-[#F24E1E] hover:dark:border-[#F24E1E]',
+      className:
+        'text-[#F24E1E] hover:dark:border-[#F24E1E] hover:bg-gradient-to-br from-gray-50/5 to-[#F24E1E]/5',
     },
     {
       title: 'JIRA',
       icon: <JiraIcon />,
       href: 'https://www.atlassian.com/software/jira',
-      className: 'text-[#0052CC] hover:dark:border-[#0052CC]',
+      className:
+        'text-[#0052CC] hover:dark:border-[#0052CC] hover:bg-gradient-to-br from-gray-50/5 to-[#0052CC]/5',
     },
     {
       title: 'Gmail',
       icon: <GmailIcon />,
       href: 'https://www.google.com/gmail/about/',
-      className: 'text-[#EA4335] hover:dark:border-[#EA4335]',
+      className:
+        'text-[#EA4335] hover:dark:border-[#EA4335] hover:bg-gradient-to-br from-gray-50/5 to-[#EA4335]/5',
     },
     {
       title: 'Google Meet',
       icon: <GoogleMeetIcon />,
       href: 'https://workspace.google.com/intl/en/products/meet/',
-      className: 'text-[#00897B] hover:dark:border-[#00897B]',
+      className:
+        'text-[#00897B] hover:dark:border-[#00897B] hover:bg-gradient-to-br from-gray-50/5 to-[#00897B]/5',
     },
     {
       title: 'Win Terminal',
       icon: <WindowsTerminalIcon />,
       href: 'https://apps.microsoft.com/detail/9n0dx20hk701?rtc=1&hl=en-eg&gl=EG',
-      className: 'text-[#fff] hover:dark:border-[#fff]',
-    },
-  ],
-  HARDWARE: [
-    {
-      title: 'Dell XPS 15 7590',
-      description:
-        'Core i7-9750H, NVIDIA GeForce GTX 1650, 32GB memory, 512MB SSD storage',
-      href: 'https://www.gadgetguy.com.au/dell-xps-15-7950-2019-a-big-laptop-for-an-even-bigger-job/',
-    },
-    {
-      title: 'Google Pixel 6a',
-      description:
-        '6.1 OLED Display, 12MP Camera, 6GB RAM, 4410mAh Battery, 128GB Storage, Android 12',
-      href: 'https://www.gsmarena.com/google_pixel_6a-11229.php',
-    },
-    {
-      title: 'Xiaomi Mi In-Ear Earphone Basic ',
-      description: 'Built-in Microphone And Silicone Ear Tips',
-      href: 'https://www.amazon.eg/-/en/Xiaomi-Earphone-Built-Microphone-Silicone/dp/B01N0Z1YKE',
+      className:
+        'text-[#fff] hover:dark:border-[#fff] hover:bg-gradient-to-br from-gray-50/5 to-[#fff]/5',
     },
   ],
 };
